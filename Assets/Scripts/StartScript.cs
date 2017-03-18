44416b8567f2a6aedc42ab520deeb0cce8e40852
oriont <elijahotarr@gmail.com>
2017-03-18 13:11:24 -0700

    initializing everything from script

--- Assets/Scripts/StartScript.cs
+++ Assets/Scripts/StartScript.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class StartScript : MonoBehaviour {
 
+    public float jumpSpeed;
+    public float bulletTime;
+    public float movementSpeed;
+    public bool lockCursor;
+
+    public float maxHealth;
+
 	// Use this for initialization
 	void Start () {
-        Instantiate(Resources.Load("Player") as GameObject);
+        GameObject player = Instantiate(Resources.Load("Player") as GameObject);
+        player.transform.position = new Vector3(0, 0.5f, 0);
+
+        var rb = player.AddComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        var health = player.AddComponent<ObjectHealth>();
+        health.maxHealth = maxHealth;
+
+        var controller = player.AddComponent<PlayerController>();
+        controller.jumpSpeed = jumpSpeed;
+        controller.bulletTime = bulletTime;
+        controller.lockCursor = lockCursor;
+        controller.movementspeed = movementSpeed;
+
+        var cam = Camera.main;
+        cam.transform.parent = player.transform;
+        cam.transform.position = new Vector3(0, 1, 0);
+
+        GameObject.Find("HUD").transform.parent = player.transform;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
